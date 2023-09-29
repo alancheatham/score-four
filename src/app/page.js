0d4dfec9f2961ad19018771b3053a1ecb6fbadfd
Alan Cheatham <alancheatham@gmail.com>
2023-09-29 19:25:26 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,94 @@
+'use client'
+
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import Pusher from 'pusher-js'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+	cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+})
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Peg({ beads, onPegClick, className }) {
+	return (
+		<div
+			className={`bg-gray-400 rounded w-8 h-28 flex flex-col-reverse items-center justify-start overflow-hidden cursor-pointer ${className}`}
+			onClick={onPegClick}
+		>
+			{beads
+				.filter((bead) => bead !== 0)
+				.map((bead, i) => (
+					<div className={`w-full h-6 ${bead === 1 ? 'bg-white' : 'bg-black'}`} key={`bead-${i}`}></div>
+				))}
+		</div>
+	)
+}
+function Grid({ board, onPegClick, winningPegs }) {
+	const pegs = []
+	for (let i = 0; i < 16; i++) {
+		pegs.push(board.slice(i * 4, i * 4 + 4))
+	}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	return (
+		<div className="grid grid-cols-4 grid-rows-4 gap-10">
+			{pegs.map((beads, i) => (
+				<Peg
+					beads={beads}
+					key={`peg-${i}`}
+					onPegClick={() => onPegClick(i)}
+					className={winningPegs.length > 0 && !winningPegs.includes(i) && 'opacity-50'}
+				/>
+			))}
+		</div>
+	)
+}
+export default function Game() {
+	const [board, setBoard] = useState(Array(64).fill(0))
+	const [whiteToMove, setWhiteToMove] = useState(false)
+	const [winner, setWinner] = useState('')
+	const [winningPegs, setWinningPegs] = useState([])
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	useEffect(() => {
+		const channel = pusher.subscribe('score-four')
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		channel.bind('move', (data) => {
+			const { board, winnerInfo } = data.message
+			setBoard(board)
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			if (winnerInfo) {
+				setWinningPegs(winnerInfo.winningPegs)
+				setWinner(winnerInfo.winner)
+			}
+		})
+
+		return () => {
+			pusher.unsubscribe('score-four')
+		}
+	}, [board])
+
+	const handlePegClick = (i) => {
+		const newBoard = [...board]
+		const peg = newBoard.slice(i * 4, i * 4 + 4)
+
+		const emptySlot = peg.findIndex((x) => x === 0)
+		if (emptySlot === -1) return
+
+		fetch('/api/move', {
+			method: 'POST',
+			body: JSON.stringify({ message: { slot: i * 4 + emptySlot }, sender: 'player' }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+
+		newBoard[i * 4 + emptySlot] = whiteToMove ? 1 : -1
+		setBoard(newBoard)
+		// setWhiteToMove(!whiteToMove)
+	}
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			{winner && <div>{winner}</div>}
+			<Grid board={board} onPegClick={handlePegClick} winningPegs={winningPegs} />
+		</main>
+	)
 }
