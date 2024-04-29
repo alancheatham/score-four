a2c5008c78d0f5443db1f2bd25c7607bc62c4ab3
Alan Cheatham <alancheatham@gmail.com>
2024-04-29 11:57:52 -0300

    smaller how to

--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -6,8 +6,8 @@ export default function HowToPlay({ onClose }) {
 
 	return (
 		<>
-			<div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" onClick={onClose}></div>
-			<div className="absolute top-0 w-full h-full sm:top-16 sm:w-[480px] sm:h-auto bg-slate-500 pt-10 pb-10 flex items-center flex-col rounded">
+			<div className="absolute top-0 left-0 w-full h-full bg-slate-500 sm:bg-black opacity-50" onClick={onClose}></div>
+			<div className="absolute top-0 w-full h-full sm:top-16 sm:w-[400px] sm:h-auto bg-slate-500 pt-10 pb-10 flex items-center flex-col rounded">
 				<button className="w-12 top-4 right-4 cursor-pointer absolute hover:opacity-50" onClick={onClose}>
 					<Image src="close.svg" alt="close" width={48} height={48}></Image>
 				</button>
@@ -15,8 +15,8 @@ export default function HowToPlay({ onClose }) {
 				<div className="text-white text-xl pb-5 text-center">Connect 4 pieces in any direction to win</div>
 				<div className="flex items-center justify-center">
 					<button
-						className={`w-12 cursor-pointer hidden sm:block hover:opacity-50 mr-5 rotate-180 ${
-							currentStep === 0 ? 'opacity-50 cursor-default' : ''
+						className={`w-12 hidden shrink-0 sm:block mr-5 rotate-180 ${
+							currentStep === 0 ? 'opacity-0 hover:opacity-0 cursor-default' : 'cursor-pointer hover:opacity-50'
 						}`}
 						disabled={currentStep === 0}
 						onClick={() => setCurrentStep(currentStep - 1)}
@@ -27,13 +27,13 @@ export default function HowToPlay({ onClose }) {
 						<img
 							src={`${index}.png`}
 							alt="board-image"
-							className={`${currentStep === index ? '' : 'hidden'}`}
+							className={`w-[200px] sm:w-[230px] ${currentStep === index ? '' : 'hidden'}`}
 							key={`tutorial-${index}`}
 						/>
 					))}
 					<button
-						className={`w-12 hidden sm:block cursor-pointer hover:opacity-50 ml-5 ${
-							currentStep === 4 ? 'opacity-50 cursor-default' : ''
+						className={`w-12 hidden shrink-0 sm:block ml-5 ${
+							currentStep === 4 ? 'opacity-0 hover:opacity-0 cursor-default' : 'cursor-pointer hover:opacity-50'
 						}`}
 						disabled={currentStep === 4}
 						onClick={() => setCurrentStep(currentStep + 1)}
@@ -43,8 +43,8 @@ export default function HowToPlay({ onClose }) {
 				</div>
 				<div className="flex sm:hidden pt-5">
 					<button
-						className={`w-12 cursor-pointer  hover:opacity-50 mr-5 rotate-180 ${
-							currentStep === 0 ? 'opacity-50 cursor-default' : ''
+						className={`w-12 mr-5 rotate-180 ${
+							currentStep === 0 ? 'opacity-0 cursor-default' : 'cursor-pointer hover:opacity-50 '
 						}`}
 						disabled={currentStep === 0}
 						onClick={() => setCurrentStep(currentStep - 1)}
@@ -53,8 +53,8 @@ export default function HowToPlay({ onClose }) {
 					</button>
 
 					<button
-						className={`w-12 cursor-pointer hover:opacity-50 ml-5 ${
-							currentStep === 4 ? 'opacity-50 cursor-default' : ''
+						className={`w-12 ml-5 ${
+							currentStep === 4 ? 'opacity-0 cursor-default' : 'cursor-pointer hover:opacity-50 '
 						}`}
 						disabled={currentStep === 4}
 						onClick={() => setCurrentStep(currentStep + 1)}
